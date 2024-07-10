c59a424f4bd918341ac0fd1dfee393599b015bf7
ary4nsinha <115373151+ary4nsinha@users.noreply.github.com>
2024-07-11 03:17:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Container from "./components/Container";
+function App() {
+  return (
+    <>
+      <Header />
+      <Container />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Container.jsx
@@ -0,0 +1,30 @@
+import {
+  FACEBOOK_MAX_CHARACTERS,
+  INSTAGRAM_MAX_CHARACTERS,
+} from "../lib/constants";
+import Stats from "./Stats";
+import Textarea from "./Textarea";
+import { useState } from "react";
+
+export default function Container() {
+  const [text, setText] = useState("");
+
+  const numberOfWords = text.split(/\s/).filter((word) => word !== "").length; // learn why this regular expression and how it works
+  const numberOfCharacters = text.length;
+  const instagramCharactersLeft = INSTAGRAM_MAX_CHARACTERS - text.length;
+  const facebookCharactersLeft = FACEBOOK_MAX_CHARACTERS - text.length;
+  const stats = {
+    numberOfWords: numberOfWords,
+    numberOfCharacters: numberOfCharacters,
+    instagramCharactersLeft: instagramCharactersLeft,
+    facebookCharactersLeft: facebookCharactersLeft,
+  };
+
+  return (
+    <main className="container">
+      <Textarea text={text} setText={setText} />
+      <Stats stats={stats} />{" "}
+      {/*before we had to write out props individually here, so we created a stats object and defined everything there and just called it here  */}
+    </main>
+  );
+}
